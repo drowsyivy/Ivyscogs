4fccb42e1649406874b3299485e74154bd78227b
drowsyivy <50467638+drowsyivy@users.noreply.github.com>
2019-05-11 12:39:17 -0400

    Update ivyscore.py
    temp test

--- ivyscore/ivyscore.py
+++ ivyscore/ivyscore.py
@@ -1109,7 +1109,7 @@ class Ivyscore:
 
     @asyncio.coroutine
     def on_message(self, message):
-        is_shiptoast = (yield from shiptoast_check((self, message)))
+        is_shiptoast = True
         if (message.author != self.bot.user) and is_shiptoast):
             if (message.content.lower().find("case in point") != -1):
                 yield from self.bot.send_message(message.channel, '\uD83D\uDC49\uD83D\uDCBC point in case')
